ecd159f52d4c95eb411af8cda96623eb37e53514
Rajat Pandit <rajat.pandit@tesco.com>
2018-08-05 12:17:19 +0530

    updated the response to include protocol buffer response for auto complete

--- routes/products-pbuff.js
+++ routes/products-pbuff.js
@@ -7,27 +7,27 @@ const es = elasticsearch.Client({
 })
 
 const protoPath = path.join(__dirname, '/..' + '/proto/' + 'product.proto');
-ProtoBuf.load(protoPath, (err, root) => {
-    if (err) 
-        throw err;
-    // obtain the message type
-    const Products = root.lookupType("AC.Products");
-    const Product = root.lookupType("AC.Product");
-    const o1 = {name: 'My Name 1', image: 'http://google.com', 'price': 12.12};
-    const o2 = {name: 'My Name 2', image: 'http://google.com', 'price': 12.12};
-    const o3 = {name: 'My Name 3', image: 'http://google.com', 'price': 12.12};        
-
-    // create the top level object
-    const products = Products.create([]);
-    // create next level and then push them into the parent object
-    products.products.push(Product.create(o1));
-    products.products.push(Product.create(o2));    
-    products.products.push(Product.create(o3));
-    // Encode a message to an Uint8Array (browser) or Buffer (node)
-    const buff = Products.encode(products).finish();
-    // decode the message, to test the output
-    console.log(Products.decode(buff));
-});
+// Reference code for future use
+// ProtoBuf.load(protoPath, (err, root) => {
+//     if (err) 
+//         throw err;
+//     // obtain the message type
+//     const Products = root.lookupType("AC.Products");
+//     const Product = root.lookupType("AC.Product");
+//     const o1 = {name: 'My Name 1', image: 'http://google.com', 'price': 12.12};
+//     const o2 = {name: 'My Name 2', image: 'http://google.com', 'price': 12.12};
+//     const o3 = {name: 'My Name 3', image: 'http://google.com', 'price': 12.12};        
+//     // create the top level object
+//     const products = Products.create([]);
+//     // create next level and then push them into the parent object
+//     products.products.push(Product.create(o1));
+//     products.products.push(Product.create(o2));    
+//     products.products.push(Product.create(o3));
+//     // Encode a message to an Uint8Array (browser) or Buffer (node)
+//     const buff = Products.encode(products).finish();
+//     // decode the message, to test the output
+//     console.log(Products.decode(buff));
+// });
 
 // TODO externalise to a config
 const INDEX_NAME = 'myindex';
@@ -70,16 +70,27 @@ const products = (req, res) => {
     getSuggestion(txt, 10)
         .then(hits => {
             let data = [];
-            hits.suggest.nameSuggester[0].options.forEach(element => {
-                data.push({
-                    name: element._source.name,
-                    image: element._source.image,
-                    price: element._source.price
+            ProtoBuf.load(protoPath, (err, root) => {
+                if (err) 
+                    throw err;
+                // obtain the message type
+                const Products = root.lookupType("AC.Products");
+                const Product = root.lookupType("AC.Product");
+                // create the top level object
+                const products = Products.create([]);
+                hits.suggest.nameSuggester[0].options.forEach(element => {
+                    products.products.push(Product.create({
+                        name: element._source.name,
+                        image: element._source.image,
+                        price: element._source.price
+                    }));
                 });
+                // Encode a message to an Uint8Array (browser) or Buffer (node)
+                const buff = Products.encode(products).finish();
+                console.log(`${hits.suggest.nameSuggester[0].options.length} results extracted for the term "${txt}"`);
+                console.log(Products.decode(buff));
+                return res.json(buff);
             });
-
-            console.log(`${data.length} results extracted for the term "${txt}"`);
-            return res.json(data);
         });
 };
 
